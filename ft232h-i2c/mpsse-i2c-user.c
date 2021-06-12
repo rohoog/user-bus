bfb2748f77136c6ffde1acc15f3603d071a90531
Ronald <ronald@bto.grundel>
2021-06-12 13:12:37 +0200

    use ioctl instead of read for the message structure

--- ft232h-i2c/mpsse-i2c-user.c
+++ ft232h-i2c/mpsse-i2c-user.c
@@ -3,17 +3,20 @@
 #include <mpsse.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/ioctl.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include "../user-i2c/user-i2c.h"
 
 /* i2c transactions should be no larger than 32 bytes, but length field could encode up to 255 */
 unsigned char buf[256];
 
 int main(void)
 {
-	int fd;
+	int fd, rc;
 	char *data = NULL;
+	struct i2c_umsg umsg;
 	struct mpsse_context *i2cbus = MPSSE(I2C, FOUR_HUNDRED_KHZ, MSB);
 
 	if (i2cbus != NULL && i2cbus->open) {
@@ -28,21 +31,30 @@ int main(void)
 		Close(i2cbus);
 		return 1;
 	}
-	while (read(fd, buf, 2)==2) {
+	while ((rc=ioctl(fd, UI2C_XFER, &umsg))>=0) {
 		int len;
+		//printf("%s %02x len %u\n", (umsg.flags&I2C_M_RD)?"read":"write", umsg.addr, umsg.len);
 		Start(i2cbus);
+		/* only 7-bit addressing for now */
+		buf[0]=(umsg.addr<<1)|((umsg.flags&I2C_M_RD)!=0);
 		Write(i2cbus, (char*)buf, 1);
 		switch (GetAck(i2cbus)) {
 			case ACK:
-				len = buf[1];
-				if (buf[0]&1) { // read
+				len = umsg.len;
+				if (umsg.flags&I2C_M_RD) { // read
 					data = Read(i2cbus, len-1);
 					if (data) {
 						memcpy(buf, data, len-1);
 						free(data);
 						SendNacks(i2cbus);
 						data = Read(i2cbus, 1);
-						buf[len-1] = *data;
+						if (data) {
+							buf[len-1] = *data;
+							free(data);
+						} else {
+							len--;
+						}
+						SendAcks(i2cbus);
 						write(fd, buf, len);
 					} else {
 						read(fd, buf, 1);
@@ -52,7 +64,9 @@ int main(void)
 					Write(i2cbus, (char*)buf, len);
 					GetAck(i2cbus);
 				}
-				Stop(i2cbus);
+				if (umsg.flags & I2C_M_STOP) {
+					Stop(i2cbus);
+				}
 				break;
 			case NACK: // receive NACK, use opposite method, data don't care
 				if (buf[0]&1) { // read
@@ -65,6 +79,7 @@ int main(void)
 				goto outloop;
 		}
 	}
+	perror("ioctl");
 outloop:
 	close(fd);
 	Close(i2cbus);
